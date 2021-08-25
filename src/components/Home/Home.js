d59848c2ee3342fa4b0e8910cb87ec049c9940a3
kevindev10 <[Kevindev1@outlook.com]>
2021-08-25 16:54:25 +0300

    We moved again, plus fixed orientation issues

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import './Home.css';
 import BannerSlider from './BannerSlider/BannerSlider';
 import responsiveWebDesign from './responsive_Web_Design.png';
 import bottomBanner from './bottomBanner.jpg';
-
+import whyus from './whyus.jpg';
 
 
 function Home() {
@@ -14,8 +14,8 @@ function Home() {
 		    </section>
 				<main>
 						<article className='pv6 first-heading-and-paragraph-on-mobile'>
-							<h1 className='tc  ph4 heading-on-mobile' style={{'fontSize':'1.6rem', }}>Web Design & Development Nairobi</h1>
-							<p className='tl pb4 ph5 paragraph-on-mobile gray  ' style={{'fontSize':'1.0rem', }} >
+							<h1 className='tc  heading-on-mobile' style={{'fontSize':'1.6rem', 'marginLeft':'8.8%', 'marginRight':'8.8%'}}>Web Design & Development Nairobi</h1>
+							<p className='tl pb4  paragraph-on-mobile gray  ' style={{'fontSize':'1.0rem','marginLeft':'8.8%', 'marginRight':'8.8%' }} >
 								We are a professional web design company based in Nairobi, Kenya.
 								We create websites uniquely crafted for your business that generate more leads, allowing you to sell your products online, and raise awareness of your brand. Our beautifully designed websites will help you stand out from the crowd and help to convert your visitors into customers.
 							</p>
@@ -23,7 +23,7 @@ function Home() {
 
 
 
-						<div className=' mb4 pv5 get-your-website-main-mobile-view ' style={			  {'backgroundColor':'#f9f9f9' }}>
+						<div className='  pv5 get-your-website-main-mobile-view ' style={			  {'backgroundColor':'#f9f9f9' }}>
 				       <article className='flex  pv4 get-your-website-flex-mobile-view' style={{'width':'82.4%', 'marginLeft':'8.8%', 'marginRight':'8.8%'}} >
 					        <h2 className=' pr4 tl get-your-website-paragraph-mobile-view ttu'  style={{'width':'40%', 'fontSize':'1.3rem', 'marginTop':'13%'}}>
 									Get a website that shows your company's products and services effectively in front of the right customers at the right time with the right message</h2>
@@ -32,6 +32,28 @@ function Home() {
 												</div>
 								</article>
 						</div>
+
+
+
+						
+							<article data-name="article-full-bleed-background">
+								<div className="cf full-bleed-image" style ={{ backgroundImage: "url("+whyus+")" }} >
+									<div className="   white full-bleed-transparent-cover " >
+										<header className="  full-bleed-text-main" >
+											<h3 className=" ttu full-bleed-text-header">WHY CHOOSE US</h3>
+											<h4 className=" full-bleed-text-paragraph-1">Hire the Best – That’s Us!</h4>
+										</header>
+										<section >
+											<p className="  full-bleed-text-paragraph-2" >
+											Our team of experienced Web Designers & Developers has a proven track record of delivering successful web solutions covering various sectors. We are always waiting for the next challenge and are always eager to learn. Before creating your website, we take the time to learn about your company.
+											</p>
+											
+										</section>
+									</div>
+								</div>
+							</article>
+					
+
 
 
 
