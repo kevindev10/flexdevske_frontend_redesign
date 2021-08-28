57bb44881160133c909f80d02af7161d85d202c7
kevindev10 <[Kevindev1@outlook.com]>
2021-08-28 15:16:18 +0300

    home page done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import ourMission from './ourMission.jpg';
+import footerBackground from './footer-background.jpg';
 import './Footer.css'
 
 function Footer() {
   return (
       <div>
          
-       	<div className="cover bg-left bg-center-l" style={{"backgroundImage": "url("+ourMission+")","height":"65vh" }}>
-									<article className="bg-black-80 " style={{"height":"65vh"}}>
+       	<div className="cover bg-left bg-center-l background-height" style={{"backgroundImage": "url("+footerBackground+")","height":"50vh" }}>
+									<article className="bg-black-80 background-cover " style={{"height":"50vh"}}>
 										
-										<section className="footer " style={{'paddingTop':'15vh',}}>
+										<section className="footer " style={{'paddingTop':'10vh',}}>
 										
                       
                         
@@ -22,7 +22,7 @@ function Footer() {
 
                             <div className='w-50-l width-100-cname'>
                               <Link exact="true" to='/' className='no-underline white'  >
-                              <p className=' ml5 mb3 footer-company-name dim pointer'  style={{ 'fontSize':'3.0rem' ,'fontFamily':'Audiowide' }}> Flex DevsKe </p>
+                              <p className=' ml5 mb3 footer-company-name dim pointer'  style={{ 'fontSize':'3.3rem' ,'fontFamily':'Audiowide' }}> Flex DevsKe </p>
                               </Link>	
                               <div className='gray ml5 footer-all-social-icons'>
                                 <a href="https://web.facebook.com/Flex-DevsKe-111123327891531" target="blank" rel="noreferrer" className='no-underline gray'>	<i className=" fab fa-facebook-square fa-4x mr3  footer-social-facebook"></i> </a>
@@ -34,19 +34,34 @@ function Footer() {
                             
 
                             <div className='w-50-r ml4   width-100-phn-no'>
-                              <a href="tel:+254789563415" className='no-underline	white phn-no fw8 dim athelas  ' style={{ 'fontSize':'3.0rem' }}>+254 789 563 415</a><br/>
-                              <p className='pv3 email-us'>
-                                <a href="mailto:sales@flexdevske.co.ke"  className='  white no-underline dim email-us-font ' style={{ 'fontSize':'25px' , }}>sales@flexdevske.co.ke </a>							
+                              <div className= ''>
+                              <a href="tel:+254789563415" className='no-underline	white phn-no  dim   ' style={{ 'fontSize':'1.8rem' }}>+254 789 563 415</a><br/>
+                              </div>
+                              
+                              <div className= 'mt2'>
+                              <a href="tel:+254721490923" className='no-underline	white phn-no dim   ' style={{ 'fontSize':'1.8rem' }}>+254 721 490 923</a><br/>
+                              </div>
+                              
+
+                              <p className='pt3 email-us'>
+                                <a href="mailto:sales@flexdevske.co.ke"  className='  white no-underline dim  email-us-font ' style={{ 'fontSize':'1.2rem' , }}>sales@flexdevske.co.ke 
+                                </a><br/>	
+                                </p>
+                              <p className='pb3 mt2 email-us'>  
+                                <a href="mailto:flexdevske@gmail.com"  className='  white no-underline dim email-us-font ' style={{ 'fontSize':'1.2rem' , }}>flexdevske@gmail.com
+                                </a>		
+
                               </p>
                               
-                              <p className='mv3 nairob-kenya white' style={{ 'fontSize':'15px'  }}>Flex DevsKe - NAIROBI, KENYA </p>
+                              
+                              <p className='mv2 nairob-kenya white' style={{ 'fontSize':'1.0rem'  }}>Flex DevsKe - NAIROBI, KENYA </p>
 
 
                               <div className='bb b--white footer-horizontal-line'></div>
                               <div className='flex footer-terms-main'>	
-                                <p className='pv3 mr4 dim pointer footer-terms white' style={{ 'fontSize':'12px' ,}}>TERMS</p>
-                                <p className='pv3 mr4 dim pointer footer-privacy white' style={{ 'fontSize':'12px' , }}>PRIVACY POLICY</p>
-                                <p className='pv3 dim pointer footer-google white' style={{ 'fontSize':'12px'  }}>OUR GOOGLE REVIEWS </p>
+                                <p className='pv3 mr4 dim pointer footer-terms white' style={{ 'fontSize':'0.8rem' ,}}>TERMS</p>
+                                <p className='pv3 mr4 dim pointer footer-privacy white' style={{ 'fontSize':'0.8rem' , }}>PRIVACY POLICY</p>
+                                <p className='pv3 dim pointer footer-google white' style={{ 'fontSize':'0.8rem'  }}>OUR GOOGLE REVIEWS </p>
                               </div>
 
                             </div>	
