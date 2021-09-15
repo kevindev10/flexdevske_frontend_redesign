7376b098e7751db0774b46f6ea9b223785e43fd6
kevindev10 <[Kevindev1@outlook.com]>
2021-09-15 14:23:54 +0300

    reworked favicon configs smh

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ function App() {
        <Navbar/>
           <Helmet>
               <meta charSet="utf-8" />
+
+
+
               <title>Flex DevsKe</title>
               <link rel="canonical" href="https://www.flexdevske.co.ke/"/>
               <meta name="description" content="We are a professional web design company based in Nairobi, Kenya. We create websites uniquely crafted for your business that generate more leads, allowing you to sell your products online, and raise awareness of your brand. Our beautifully designed websites will help you stand out from the crowd and help to convert your visitors into customers." />
--- src/components/WhatsappButton/WhatsappButton.js
+++ src/components/WhatsappButton/WhatsappButton.js
@@ -4,9 +4,9 @@ import './WhatsappButton.css'
 
 function WhatsappButton (){
   return(
-    <div>
-        <div class="" >
-          {/* <Link to='/contact'> <button className='footer-icon-carevolution buttonMiddle-styling2 dib f3 pa3 grow fw8  white ' >GET A QUOTE</button> </Link> */}
+    <>
+        
+         
           <a href="https://wa.me/message/O2J5L6G63TQXB1" target="blank" rel="noreferrer" className='no-underline ' aria-label="The Flex DevsKe WhatsApp chat link">
             
           <i className="fab fa-whatsapp fa-4x footer-icon-carevolution  green " aria-hidden="true"></i>
@@ -14,8 +14,8 @@ function WhatsappButton (){
           </a>
 
           	
-        </div>
-    </div>
+        
+    </>
   )
 }
 
